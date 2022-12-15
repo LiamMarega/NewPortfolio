ae8060181d0764f9575d2a989f7a9941e80a9aa9
LiamMarega <liammarega85@gmail.com>
2022-12-15 12:31:20 -0300

    adjust

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Paper } from '@mui/material';
+import {Box, Paper} from '@mui/material';
 import React from 'react';
 import Loading from '../../components/Loading';
 import BasePages from '../Base';
