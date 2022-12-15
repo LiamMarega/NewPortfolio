50dd44cf882ade1de07011f91f63568096f89054
liammarega <liammarega85@gmail.com>
2022-12-15 12:30:23 -0300

    changes about

--- src/pages/Home/parts/About/index.jsx
+++ src/pages/Home/parts/About/index.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import Grid2 from '@mui/material/Unstable_Grid2';
-import { Box } from '@mui/material';
+import { Box, Stack } from '@mui/material';
 import './styles.scss';
+import AccordionComponent from '../../../../components/Accordion';
+import { tech1, tech2 } from '../../../../utils/Jsons/techs.jsx';
 
 function AboutHome() {
+  console.log(tech1);
+
   return (
     <Box className="ContainerAboutHome">
       <Grid2 container spacing={2}>
         <Grid2
           xs={6}
           display="flex"
+          flexDirection={{ xs: 'column', md: 'column' }}
           justifyContent="center"
           alignItems="center"
         >
@@ -21,7 +26,22 @@ function AboutHome() {
             I graduated from a full-stack coding Bootcamp and want to start
             developing my career in the tech world.
           </p>
+          <p>
+            An Empathic, enthusiastic person, challenge-lover, and someone that
+            always wants something new to learn about, I love to take collective
+            property of the project that I’m working on and make sure that the
+            result is the best one that it could’ve been.
+          </p>
+          <p>
+            I’m a front-end web developer from Argentina, who enjoys the
+            creation of good-looking content living on the internet. I have
+            always been passionate about technology and ever since I finished
+            high school I began to learn how to code right at college. Recently,
+            I graduated from a full-stack coding Bootcamp and want to start
+            developing my career in the tech world.
+          </p>
         </Grid2>
+
         <Grid2
           xs={5}
           display="flex"
@@ -32,6 +52,14 @@ function AboutHome() {
             src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&w=1000&q=80"
             alt="developerIMG"
           />
+        </Grid2>
+      </Grid2>
+      <Grid2 container spacing={5}>
+        <Grid2 xs={5}>
+          <AccordionComponent data={tech1} />
+        </Grid2>
+        <Grid2 xs={5}>
+          <AccordionComponent data={tech2} />
         </Grid2>
       </Grid2>
     </Box>
---
+++ src/utils/Jsons/techs.jsx
@@ -0,0 +1,48 @@
+export const tech1 = [
+  {
+    tech: 'JavaScript',
+    time: '2 years',
+    expertise: 'Advanced',
+    description:
+      'JavaScript is a programming language that is one of the core technologies of the World Wide Web, along with HTML and CSS.'
+  },
+  {
+    tech: 'Flutter',
+    time: '1 year',
+    expertise: 'Medium',
+    description:
+      'Flutter is an open source mobile app development SDK created by Google.'
+  },
+  {
+    tech: 'React',
+    time: '2 years',
+    expertise: 'Advanced',
+    description:
+      'React is an open source Javascript library designed to create user interfaces to make it easy to develop single page applications.'
+  }
+];
+
+export const tech2 = [
+  {
+    tech: 'MongoDB',
+    time: '1 years',
+    expertise: 'Medium',
+    description:
+      'MongoDB is an open source, document-oriented, NoSQL database system.'
+  },
+  {
+    tech: 'Node.js',
+    time: '2 years',
+    expertise: 'Medium',
+    description: 'Node.js is an open source server environment.'
+  },
+  {
+    tech: 'TypeScript',
+    time: '1 years',
+    expertise: 'Medium',
+    description:
+      'TypeScript is a free and open source programming language developed and maintained by Microsoft.'
+  }
+];
+
+export default { tech1, tech2 };
